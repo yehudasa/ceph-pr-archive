43d5252f1814f4c6bd036513253fa31ad3ad99b7
John Spray <john.spray@redhat.com>
2018-04-03 12:19:43 +0100

    mgr: expose OSDMap.pool_raw_used_rate
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/osd/OSDMap.h
+++ src/osd/OSDMap.h
@@ -1467,6 +1467,9 @@ public:
   int parse_osd_id_list(const vector<string>& ls,
 			set<int> *out,
 			ostream *ss) const;
+
+  float pool_raw_used_rate(int64_t poolid) const;
+
 };
 WRITE_CLASS_ENCODER_FEATURES(OSDMap)
 WRITE_CLASS_ENCODER_FEATURES(OSDMap::Incremental)
