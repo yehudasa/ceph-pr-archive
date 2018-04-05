516b015f0661d79d628481ddf8f0dffe324d1fb5
Venky Shankar <vshankar@redhat.com>
2018-04-05 06:39:52 -0400

    rbd: track async journal operations and serialize destruction
    With configurable worker threads, an object could be destructed
    when its still being used. Typically this is triggered when an
    asynchronous function completes context callback but continues
    execution with the context callback freeing up the object. Much
    of this is possibly code written under the assumption of a single
    worker thread (thereby the callback will only execute after the
    current thread has finished processing).
    
    According to test results, there is just one single class that
    exhibits the above pattern. Therefore, the fix is kept pretty
    simple by serializing the object destruction with possible
    unfinished asynchronous operations.
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/librbd/Journal.cc
+++ src/librbd/Journal.cc
@@ -351,6 +351,7 @@ Journal<I>::~Journal() {
     delete m_work_queue;
   }
 
+  Mutex::Locker locker(m_lock);
   ceph_assert(m_state == STATE_UNINITIALIZED || m_state == STATE_CLOSED);
   ceph_assert(m_journaler == NULL);
   ceph_assert(m_journal_replay == NULL);
@@ -1222,6 +1223,8 @@ void Journal<I>::handle_replay_ready() {
     m_processing_entry = true;
   }
 
+  m_async_journal_op_tracker.start_op();
+
   bufferlist data = replay_entry.get_data();
   auto it = data.cbegin();
 
@@ -1283,11 +1286,15 @@ void Journal<I>::handle_replay_complete(int r) {
       // ensure the commit position is flushed to disk
       m_journaler->flush_commit_position(ctx);
     });
+  ctx = new FunctionContext([this, ctx](int r) {
+      m_async_journal_op_tracker.wait(m_image_ctx, ctx);
+    });
   ctx = new FunctionContext([this, cct, cancel_ops, ctx](int r) {
       ldout(cct, 20) << this << " handle_replay_complete: "
                      << "shut down replay" << dendl;
       m_journal_replay->shut_down(cancel_ops, ctx);
     });
+
   m_journaler->stop_replay(ctx);
 }
 
@@ -1346,11 +1353,13 @@ void Journal<I>::handle_replay_process_safe(ReplayEntry replay_entry, int r) {
           m_journal_replay->shut_down(true, ctx);
         });
       m_journaler->stop_replay(ctx);
+      m_async_journal_op_tracker.finish_op();
       return;
     } else if (m_state == STATE_FLUSHING_REPLAY) {
       // end-of-replay flush in-progress -- we need to restart replay
       transition_state(STATE_FLUSHING_RESTART, r);
       m_lock.Unlock();
+      m_async_journal_op_tracker.finish_op();
       return;
     }
   } else {
@@ -1358,6 +1367,7 @@ void Journal<I>::handle_replay_process_safe(ReplayEntry replay_entry, int r) {
     m_journaler->committed(replay_entry);
   }
   m_lock.Unlock();
+  m_async_journal_op_tracker.finish_op();
 }
 
 template <typename I>
