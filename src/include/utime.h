719708f816597d3ae0eb731fe355fceb0319e373
Mohamad Gebai <mgebai@suse.com>
2018-05-24 12:33:00 -0400

    tracing: add operator string() to utime_t
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/include/utime.h
+++ src/include/utime.h
@@ -205,6 +205,12 @@ public:
     return ts;
   }
 
+  operator std::string() {
+    std::stringstream out;
+    out << this;
+    return out.str();
+  }
+
   void sleep() const {
     struct timespec ts;
     to_timespec(&ts);
