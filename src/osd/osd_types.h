a70ddc8b8b17fcca2bbc64c053ef48bd6b8c97cc
Mohamad Gebai <mgebai@suse.com>
2018-05-31 13:27:44 -0400

    tracing: add operator string() implementations
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -761,6 +761,18 @@ public:
     return 0;  // whatever.
   }
 
+  operator std::string() {
+    std::stringstream out;
+    out << this;
+    return out.str();
+  }
+
+  operator std::string() const {
+    std::stringstream out;
+    out << this;
+    return out.str();
+  }
+
   void dump(Formatter *f) const;
   static void generate_test_instances(list<coll_t*>& o);
 };
