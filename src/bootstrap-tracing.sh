a9a1309613833e6f6e636ce6d1f284e8b0a98030
Mohamad Gebai <mgebai@suse.com>
2018-06-06 21:32:46 -0400

    tracing: make paths conifgurable in bootstrap-tracing.sh
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/bootstrap-tracing.sh
+++ src/bootstrap-tracing.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
+# OUTDIR=tracing
+OUTDIR=../build/tracing
+INCLDIR=../build/include/tracing
+
 for f in `find tracing/tracetool/subsys -type f`; do
   # uncomment to use lttng
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t tp > tracing/`basename ${f}`.tp
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t h > tracing/`basename ${f}`_impl.h
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t c > tracing/`basename ${f}`.c
+  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t tp > ${OUTDIR}/`basename ${f}`.tp
+  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t h > ${INCLDIR}/`basename ${f}`_impl.h
+  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t c > ${OUTDIR}/`basename ${f}`.c
 
   # uncomment to use dout
-  #python tracing/tracetool/tracetool.py -f ${f} -b dout -t h > tracing/`basename ${f}`_impl.h
+  #python tracing/tracetool/tracetool.py -f ${f} -b dout -t h > ${OUTDIR}/`basename ${f}`_impl.h
 done
 
