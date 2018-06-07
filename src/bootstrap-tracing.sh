44fcac6f7b916dd0937611a5bcbe019af0dc0ccc
Mohamad Gebai <mgebai@suse.com>
2018-06-07 11:12:19 -0400

    tracing: add parameter for bootstrap-tracing to choose backend
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/bootstrap-tracing.sh
+++ src/bootstrap-tracing.sh
@@ -4,13 +4,25 @@
 OUTDIR=../build/tracing
 INCLDIR=../build/include/tracing
 
+if [ $# -eq 1 ] ; then
+  BACKEND=$1
+else
+  BACKEND=lttng
+fi
+
+echo "Generating for $BACKEND"
+
 for f in `find tracing/tracetool/subsys -type f`; do
   # uncomment to use lttng
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t tp > ${OUTDIR}/`basename ${f}`.tp
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t h > ${INCLDIR}/`basename ${f}`_impl.h
-  python tracing/tracetool/tracetool.py -f ${f} -b lttng -t c > ${OUTDIR}/`basename ${f}`.c
-
+  if [ $BACKEND == lttng ] ; then
+    python tracing/tracetool/tracetool.py -f ${f} -b lttng -t tp > ${OUTDIR}/`basename ${f}`.tp
+    python tracing/tracetool/tracetool.py -f ${f} -b lttng -t h > ${INCLDIR}/`basename ${f}`_impl.h
+    python tracing/tracetool/tracetool.py -f ${f} -b lttng -t c > ${OUTDIR}/`basename ${f}`.c
+  else
   # uncomment to use dout
-  #python tracing/tracetool/tracetool.py -f ${f} -b dout -t h > ${OUTDIR}/`basename ${f}`_impl.h
+    rm -f ${OUTDIR}/`basename ${f}`.tp
+    rm -f ${OUTDIR}/`basename ${f}`.c
+    python tracing/tracetool/tracetool.py -f ${f} -b dout -t h > ${INCLDIR}/`basename ${f}`_impl.h
+  fi
 done
 
