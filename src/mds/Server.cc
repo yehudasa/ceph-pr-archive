batrick/ceph

mds: support MDentryLink and MDentryUnlink
Signed-off-by: dongdong tao <tdd21151186@gmail.com>

src/mds/Server.cc
@@ -1918,9 +1918,9 @@ void Server::handle_slave_request(MMDSSlaveRequest *m)
     return;
   }
 
-  CDentry *straydn = NULL;
+  CDentry *straydn = nullptr;
   if (m->stray.length() > 0) {
-    straydn = mdcache->add_replica_stray(m->stray, from);
+    mdcache->decode_replica_stray(straydn, m->stray, from);
     assert(straydn);
     m->stray.clear();
   }
@@ -6205,7 +6205,7 @@ bool Server::_rmdir_prepare_witness(MDRequestRef& mdr, mds_rank_t who, vector<CD
   req->srcdnpath = filepath(trace.front()->get_dir()->ino());
   for (auto dn : trace)
     req->srcdnpath.push_dentry(dn->get_name());
-  mdcache->replicate_stray(straydn, who, req->stray);
+  mdcache->encode_replica_stray(straydn, who, req->stray);
 
   req->op_stamp = mdr->get_op_stamp();
   mds->send_message_mds(req, who);
@@ -7064,7 +7064,7 @@ bool Server::_rename_prepare_witness(MDRequestRef& mdr, mds_rank_t who, set<mds_
   for (auto dn : dsttrace)
     req->destdnpath.push_dentry(dn->get_name());
   if (straydn)
-    mdcache->replicate_stray(straydn, who, req->stray);
+    mdcache->encode_replica_stray(straydn, who, req->stray);
 
   req->srcdn_auth = mdr->more()->srcdn_auth_mds;
   

yehudasa/ceph-pr-archive

src/mds/Server.cc
@@ -1965,7 +1965,7 @@ void Server::handle_slave_request(MMDSSlaveRequest *m)
 
   CDentry *straydn = NULL;
   if (m->straybl.length() > 0) {
-    straydn = mdcache->add_replica_stray(m->straybl, from);
+    mdcache->decode_replica_stray(straydn, m->straybl, from);
     assert(straydn);
     m->straybl.clear();
   }
@@ -6429,7 +6429,7 @@ bool Server::_rmdir_prepare_witness(MDRequestRef& mdr, mds_rank_t who, vector<CD
   req->srcdnpath = filepath(trace.front()->get_dir()->ino());
   for (auto dn : trace)
     req->srcdnpath.push_dentry(dn->get_name());
-  mdcache->replicate_stray(straydn, who, req->straybl);
+  mdcache->encode_replica_stray(straydn, who, req->straybl);
   if (mdr->more()->desti_srnode)
     encode(*mdr->more()->desti_srnode, req->desti_snapbl);
 
@@ -7356,8 +7356,8 @@ bool Server::_rename_prepare_witness(MDRequestRef& mdr, mds_rank_t who, set<mds_
   for (auto dn : dsttrace)
     req->destdnpath.push_dentry(dn->get_name());
   if (straydn)
-    mdcache->replicate_stray(straydn, who, req->straybl);
-
+    mdcache->encode_replica_stray(straydn, who, req->straybl);
+  
   if (mdr->more()->srci_srnode)
     encode(*mdr->more()->srci_srnode, req->srci_snapbl);
   if (mdr->more()->desti_srnode)