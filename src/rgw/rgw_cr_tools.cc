yehudasa/ceph

rgw: new cr for simple object put
Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_cr_tools.cc
@@ -220,3 +220,22 @@ int RGWBucketCreateLocalCR::Request::_send_request()
 
   return ret;
 }
+
+template<>
+int RGWObjectSimplePut::Request::_send_request()
+{
+  RGWDataAccess::ObjectRef obj;
+
+  int ret = params.bucket->get_object(params.key, &obj);
+  if (ret < 0) {
+    cerr << "ERROR: failed to get object: " << cpp_strerror(-ret) << std::endl;
+    return -ret;
+  }
+
+  ret = obj->put(params.data, params.attrs);
+  if (ret < 0) {
+    cerr << "ERROR: put object returned error: " << cpp_strerror(-ret) << std::endl;
+  }
+
+  return 0;
+}

src/rgw/rgw_cr_tools.h
@@ -2,6 +2,7 @@
 #define CEPH_RGW_CR_TOOLS_H
 
 #include "rgw_cr_rados.h"
+#include "rgw_tools.h"
 
 
 struct rgw_user_create_params {
@@ -48,6 +49,15 @@ struct rgw_bucket_create_local_params {
 };
 
 using RGWBucketCreateLocalCR = RGWSimpleWriteOnlyAsyncCR<rgw_bucket_create_local_params>;
+
+struct rgw_object_simple_put_params {
+  RGWDataAccess::BucketRef bucket;
+  rgw_obj_key key;
+  bufferlist data;
+  map<string, bufferlist> attrs;
+};
+
+using RGWObjectSimplePut = RGWSimpleWriteOnlyAsyncCR<rgw_object_simple_put_params>;
 
 
 

yehudasa/ceph-pr-archive

src/rgw/rgw_cr_tools.cc
@@ -222,3 +222,22 @@ int RGWBucketCreateLocalCR::Request::_send_request()
 
   return ret;
 }
+
+template<>
+int RGWObjectSimplePutCR::Request::_send_request()
+{
+  RGWDataAccess::ObjectRef obj;
+
+  int ret = params.bucket->get_object(params.key, &obj);
+  if (ret < 0) {
+    cerr << "ERROR: failed to get object: " << cpp_strerror(-ret) << std::endl;
+    return -ret;
+  }
+
+  ret = obj->put(params.data, params.attrs);
+  if (ret < 0) {
+    cerr << "ERROR: put object returned error: " << cpp_strerror(-ret) << std::endl;
+  }
+
+  return 0;
+}

src/rgw/rgw_cr_tools.h
@@ -2,6 +2,7 @@
 #define CEPH_RGW_CR_TOOLS_H
 
 #include "rgw_cr_rados.h"
+#include "rgw_tools.h"
 
 
 struct rgw_user_create_params {
@@ -49,6 +50,15 @@ struct rgw_bucket_create_local_params {
 };
 
 using RGWBucketCreateLocalCR = RGWSimpleWriteOnlyAsyncCR<rgw_bucket_create_local_params>;
+
+struct rgw_object_simple_put_params {
+  RGWDataAccess::BucketRef bucket;
+  rgw_obj_key key;
+  bufferlist data;
+  map<string, bufferlist> attrs;
+};
+
+using RGWObjectSimplePutCR = RGWSimpleWriteOnlyAsyncCR<rgw_object_simple_put_params>;
 
 
 