30663a378a5d70be4e5816b4d9fce0699c812db5
Tatjana Dehler <tdehler@suse.com>
2018-10-19 16:02:15 +0200

    mgr/dashboard: move config option types to own file
    Move the known config option types to their own file so that the
    information can be shared with other components.
    
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/configuration/configuration-form/configuration-form.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/configuration/configuration-form/configuration-form.component.ts
@@ -11,6 +11,7 @@ import { CdValidators } from '../../../../shared/forms/cd-validators';
 import { NotificationService } from '../../../../shared/services/notification.service';
 import { ConfigFormCreateRequestModel } from './configuration-form-create-request.model';
 import { ConfigFormModel } from './configuration-form.model';
+import { ConfigOptionTypes } from './configuration-form.types';
 
 @Component({
   selector: 'cd-configuration-form',
@@ -68,72 +69,9 @@ export class ConfigurationFormComponent implements OnInit {
   }
 
   getType(type: string): any {
-    const knownTypes = [
-      {
-        name: 'uint64_t',
-        inputType: 'number',
-        humanReadable: 'Positive integer value',
-        defaultMin: 0,
-        patternHelpText: 'The entered value needs to be a positive number.',
-        isNumberType: true,
-        allowsNegative: false
-      },
-      {
-        name: 'int64_t',
-        inputType: 'number',
-        humanReadable: 'Integer value',
-        patternHelpText: 'The entered value needs to be a number.',
-        isNumberType: true,
-        allowsNegative: true
-      },
-      {
-        name: 'size_t',
-        inputType: 'number',
-        humanReadable: 'Positive integer value (size)',
-        defaultMin: 0,
-        patternHelpText: 'The entered value needs to be a positive number.',
-        isNumberType: true,
-        allowsNegative: false
-      },
-      {
-        name: 'secs',
-        inputType: 'number',
-        humanReadable: 'Positive integer value (secs)',
-        defaultMin: 1,
-        patternHelpText: 'The entered value needs to be a positive number.',
-        isNumberType: true,
-        allowsNegative: false
-      },
-      {
-        name: 'double',
-        inputType: 'number',
-        humanReadable: 'Decimal value',
-        patternHelpText: 'The entered value needs to be a number or decimal.',
-        isNumberType: true,
-        allowsNegative: true
-      },
-      { name: 'std::string', inputType: 'text', humanReadable: 'Text', isNumberType: false },
-      {
-        name: 'entity_addr_t',
-        inputType: 'text',
-        humanReadable: 'IPv4 or IPv6 address',
-        patternHelpText: 'The entered value needs to be a valid IP address.',
-        isNumberType: false
-      },
-      {
-        name: 'uuid_d',
-        inputType: 'text',
-        humanReadable: 'UUID',
-        patternHelpText:
-          'The entered value is not a valid UUID, e.g.: 67dcac9f-2c03-4d6c-b7bd-1210b3a259a8',
-        isNumberType: false
-      },
-      { name: 'bool', inputType: 'checkbox', humanReadable: 'Boolean value', isNumberType: false }
-    ];
-
     let currentType = null;
 
-    knownTypes.forEach((knownType) => {
+    ConfigOptionTypes.KNOWN_TYPES.forEach((knownType) => {
       if (knownType.name === type) {
         currentType = knownType;
       }
