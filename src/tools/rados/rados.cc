sunnyku/ceph-ci

rados,mon: improve ceph and rados df reports.
Fixes: https://tracker.ceph.com/issues/22159
Fixes: https://tracker.ceph.com/issues/20870

This adds  more parameters to estimate allocation granularity overhead and compression
effectiveness. RAW_USED column has been removed. USED column reflects
amount of space allocated for a pool at all replicas. Including
allocaction granularity overhead and taking ompression savings and
object content gaps into account.

Following columns were appended:
* STORED - approximation of bytes users've actually stored in a
pool, i.e netto data amount without compression, allocation and
other overheads (not applicable for 'rados df').
* USED COMPR - amount of bytes allocated to store compressed data, i.e.
compressed data plus allocation/replication overhead.
* UNDER COMPR - amount of data(icluding repicated one) passed through
compression and stored in that form.

Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/tools/rados/rados.cc
@@ -2281,6 +2281,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
       tab.define_column("RD", TextTable::RIGHT, TextTable::RIGHT);
       tab.define_column("WR_OPS", TextTable::RIGHT, TextTable::RIGHT);
       tab.define_column("WR", TextTable::RIGHT, TextTable::RIGHT);
+      tab.define_column("USED COMPR", TextTable::RIGHT, TextTable::RIGHT);
+      tab.define_column("UNDER COMPR", TextTable::RIGHT, TextTable::RIGHT);
     } else {
       formatter->open_object_section("stats");
       formatter->open_array_section("pools");
@@ -2303,6 +2305,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
             << byte_u_t(s.num_rd_kb << 10)
             << s.num_wr
             << byte_u_t(s.num_wr_kb << 10)
+	    << byte_u_t(s.compressed_bytes_alloc)
+	    << byte_u_t(s.compressed_bytes_orig)
             << TextTable::endrow;
       } else {
         formatter->open_object_section("pool");
@@ -2325,6 +2329,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
 	formatter->dump_int("read_bytes", s.num_rd_kb * 1024ull);
 	formatter->dump_int("write_ops", s.num_wr);
 	formatter->dump_int("write_bytes", s.num_wr_kb * 1024ull);
+	formatter->dump_int("bytes_compressed", s.compressed_bytes_alloc);
+	formatter->dump_int("bytes_under_compress", s.compressed_bytes_orig);
 	formatter->close_section();
       }
     }

yehudasa/ceph-pr-archive

src/tools/rados/rados.cc
@@ -2281,6 +2281,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
       tab.define_column("RD", TextTable::RIGHT, TextTable::RIGHT);
       tab.define_column("WR_OPS", TextTable::RIGHT, TextTable::RIGHT);
       tab.define_column("WR", TextTable::RIGHT, TextTable::RIGHT);
+      tab.define_column("USED COMPR", TextTable::RIGHT, TextTable::RIGHT);
+      tab.define_column("UNDER COMPR", TextTable::RIGHT, TextTable::RIGHT);
     } else {
       formatter->open_object_section("stats");
       formatter->open_array_section("pools");
@@ -2303,6 +2305,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
             << byte_u_t(s.num_rd_kb << 10)
             << s.num_wr
             << byte_u_t(s.num_wr_kb << 10)
+	    << byte_u_t(s.compressed_bytes_alloc)
+	    << byte_u_t(s.compressed_bytes_orig)
             << TextTable::endrow;
       } else {
         formatter->open_object_section("pool");
@@ -2325,6 +2329,8 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
 	formatter->dump_int("read_bytes", s.num_rd_kb * 1024ull);
 	formatter->dump_int("write_ops", s.num_wr);
 	formatter->dump_int("write_bytes", s.num_wr_kb * 1024ull);
+	formatter->dump_int("compress_bytes_used", s.compressed_bytes_alloc);
+	formatter->dump_int("compress_under_bytes", s.compressed_bytes_orig);
 	formatter->close_section();
       }
     }