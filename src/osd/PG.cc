samchoraria/ceph-ci

osd: Improve SnapMapper handling of inconsistencies
My using nop() instead of setattr() we avoid crashing
a replica that is missing the object for some reason.
Although this is a corruption, it would be getting "fix"
by removal of the snapshot.

In the other cases we accept completely missing keys (ENOENT)
that happens when an object is removed somehow or missed
by recovery/backfill.

Signed-off-by: David Zafman <dzafman@redhat.com>

src/osd/PG.cc
@@ -3851,7 +3851,10 @@ void PG::update_snap_map(
 	int r = snap_mapper.remove_oid(
 	  i->soid,
 	  &_t);
-	assert(r == 0);
+	if (r != 0)
+	  derr << __func__ << " remove_oid " << i->soid << " failed with " << r << dendl;
+        // On removal tolerate missing key corruption
+        assert(r == 0 || r == -ENOENT);
       } else if (i->is_update()) {
 	assert(i->snaps.length() > 0);
 	vector<snapid_t> snaps;

yehudasa/ceph-pr-archive

src/osd/PG.cc
@@ -4015,7 +4015,10 @@ void PG::update_snap_map(
 	int r = snap_mapper.remove_oid(
 	  i->soid,
 	  &_t);
-	ceph_assert(r == 0);
+	if (r != 0)
+	  derr << __func__ << " remove_oid " << i->soid << " failed with " << r << dendl;
+        // On removal tolerate missing key corruption
+        ceph_assert(r == 0 || r == -ENOENT);
       } else if (i->is_update()) {
 	ceph_assert(i->snaps.length() > 0);
 	vector<snapid_t> snaps;