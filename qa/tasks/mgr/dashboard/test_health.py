2ec71d78c015f793d7d6dbd2992a08a5d378e8b2
Zack Cerza <zack@redhat.com>
2018-11-06 17:15:24 -0700

    review changes: test minimal health with json schema (full health needs this change too)

--- qa/tasks/mgr/dashboard/test_health.py
+++ qa/tasks/mgr/dashboard/test_health.py
@@ -1,46 +1,79 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .helper import DashboardTestCase
+from .helper import DashboardTestCase, JLeaf, JList, JObj, JAny
 
 
 class HealthTest(DashboardTestCase):
     CEPHFS = True
 
     def test_minimal_health(self):
-        data = self._get("/api/health/minimal")
+        data = self._get('/api/health/minimal')
         self.assertStatus(200)
-
-        self.assertNotIn('audit_log', data)
-        self.assertIn('client_perf', data)
-        self.assertSortedEqual(
-            data['client_perf'].keys(),
-            ['read_bytes_sec', 'read_op_per_sec',
-             'recovering_bytes_per_sec', 'write_bytes_sec',
-             'write_op_per_sec']
-        )
-        self.assertNotIn('clog', data)
-        self.assertIn('df', data)
-        self.assertEqual(data['df'].keys(), ['stats'])
-        self.assertSortedEqual(
-            data['df']['stats'].keys(),
-            ['total_avail_bytes', 'total_bytes', 'total_objects',
-             'total_used_bytes']
-        )
-        self.assertIn('fs_map', data)
-        self.assertSortedEqual(
-            data['fs_map'].keys(), ['filesystems', 'standbys'])
-        for fs in data['fs_map']['filesystems']:
-            self.assertEqual(fs.keys(), ['mdsmap'])
-            self.assertEqual(fs['mdsmap'].keys(), ['info'])
-            for item in fs['mdsmap']['info'].values():
-                self.assertEqual(item.keys(), ['state'])
-        self.assertIn('health', data)
-        self.assertSortedEqual(data['health'].keys(), ['checks', 'status'])
-        self.assertIn('hosts', data)
+        spec = JObj({
+            'client_perf': JObj({
+                'read_bytes_sec': int,
+                'recovering_bytes_per_sec': int,
+                'write_op_per_sec': int,
+                'write_bytes_sec': int,
+                'read_op_per_sec': int,
+            }),
+            'df': JObj({
+                'stats': JObj({
+                    'total_objects': int,
+                    'total_used_bytes': int,
+                    'total_bytes': int,
+                    'total_avail_bytes': int,
+                })
+            }),
+            'osd_map': JObj({
+                'osds': JList(
+                    JObj({
+                        'in': int,
+                        'up': int,
+                    })),
+            }),
+            'rgw': int,
+            'fs_map': JObj({
+                'standbys': JList(JObj({})),
+                'filesystems': JList(
+                    JObj({
+                        # TODO: Find a way to verify the structure of
+                        # mdsmap['info']; it doesn't appear there is a way to
+                        # specify that a dict can have any string as a *key*.
+                        'mdsmap': JAny(none=False)
+                    }),
+                )
+            }),
+            'pg_info': JObj({
+                'pgs_per_osd': int,
+                # TODO: Find a way to verify that each key under 'statuses' has
+                # an integer value
+                'statuses': JObj({}, allow_unknown=True)
+            }),
+            'mon_status': JObj({
+                'monmap': JObj({
+                    'mons': JList(JLeaf(dict)),
+                }),
+                'quorum': JList(int)
+            }),
+            'health': JObj({
+                'status': str,
+                'checks': JList(str)
+            }),
+            'scrub_status': str,
+            'pools': JList(JLeaf(dict)),
+            'hosts': int,
+            'iscsi_daemons': int,
+            'mgr_map': JObj({
+                'standbys': JList(JLeaf(dict)),
+                'active_name': str
+            })
+        })
+        self.assertSchema(data, spec)
 
     def test_full_health(self):
-        data = self._get("/api/health/full")
+        data = self._get('/api/health/full')
         self.assertStatus(200)
 
         self.assertIn('health', data)
@@ -80,7 +113,7 @@ class HealthTest(DashboardTestCase):
 
     @DashboardTestCase.RunAs('test', 'test', ['pool-manager'])
     def test_health_permissions(self):
-        data = self._get("/api/health/full")
+        data = self._get('/api/health/full')
         self.assertStatus(200)
 
         self.assertIn('health', data)
